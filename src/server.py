ec7eaf63eb2129dde91a0825dcb40cdd508440da
rialda <rialda.spahic@gmail.com>
2018-03-23 22:13:25 +0100

    Routes
    source Routes

--- src/server.py
+++ src/server.py
@@ -3,15 +3,28 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# @ signifies a decorator - for wrapping function and modifying it's behavior
+# @ map a url to a return value
 @app.route('/')
-def hello_world():
-    return 'Hello, lalal!'
+def index():
+    return 'Landing page'
 
 
 @app.route('/home')
 def home():
-    return "This is home! "
+    return '<h1> Home Page </h1>'
 
 
+@app.route('/data/<source>')
+def data(source):
+    return "Requesting <i> %s </i> data" % source
+
+
+@app.route('/number/<int:num>')
+def number(num):
+    return "Number is %s " % num
+
+
+# make sure to start the web server whenever this file is called directly
 if __name__ == "__main__":
     app.run(debug=True)
